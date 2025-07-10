560094a59e5dde850767ff5b490b928f422092ef
hikki8man <johan.chevet@hotmail.fr>
2025-07-10 20:26:11 +0200

    eod

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -6,19 +6,18 @@
     <link rel="stylesheet" href="style.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="./script.js"></script>
+    <title>Job02</title>
   </head>
   <body>
     <button id="btn-shuffle">Shuffle</button>
-    <!-- <div class="game"> -->
       <div id="items-placed"></div>
       <div id="items">
-        <img id="1" src="./img/arc1.webp">
-        <img id="2" src="./img/arc2.webp">
-        <img id="3" src="./img/arc3.webp">
-        <img id="4" src="./img/arc4.webp">
-        <img id="5" src="./img/arc5.webp">
-        <img id="6" src="./img/arc6.webp">
+        <img id="1" src="./img/arc1.webp" alt="image 1">
+        <img id="2" src="./img/arc2.webp" alt="image 2">
+        <img id="3" src="./img/arc3.webp" alt="image 3">
+        <img id="4" src="./img/arc4.webp" alt="image 4">
+        <img id="5" src="./img/arc5.webp" alt="image 5">
+        <img id="6" src="./img/arc6.webp" alt="image 6">
       </div>
-    <!-- </div> -->
   </body>
   </html>
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,24 +1,32 @@
 $(document).ready(function () {
   $("#btn-shuffle").on("click", function () {
-    let parent = $("#items");
-    let divs = parent.children();
-    while (divs.length) {
-      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    let items = $("#items");
+    let rainbows = items.children();
+    while (rainbows.length) {
+      items.append(rainbows.splice(Math.floor(Math.random() * rainbows.length), 1)[0]);
     }
   });
   $("#items > img").on("click", function () {
+    let inPlace = true;
     let itemsPlacedBox = $("#items-placed");
     // Unbind click event
     let imgSelected = $(this).off();
-    console.log(imgSelected);
     itemsPlacedBox.append(imgSelected);
     // console.log(itemsPlacedBox.length);
     if (itemsPlacedBox.children().length === 6) {
       // check order
       for (let i = 0; i < itemsPlacedBox.children().length; i++) {
-        console.log(itemsPlacedBox.children()[i]);
-        // if (itemsPlacedBox)
+        let rainbow = $(itemsPlacedBox.children()[i]);
+        if (rainbow.attr("id") != i + 1) {
+          inPlace = false;
+        }
       }
+      if (inPlace) {
+        // add winner text
+      } else {
+        // add loser text
+      }
+      console.log("In place: ", inPlace)
     }
   });
 });
