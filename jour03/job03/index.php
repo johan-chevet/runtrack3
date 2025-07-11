c6f9c2935bf6f2880cb28c065df28b8bae912ba2
Johan <johan.chevet@laplateforme.io>
2025-07-11 16:38:41 +0200

    eod

---
+++ jour03/job03/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./script.js"></script>
+    <title>Job03</title>
+  </head>
+  <body>
+    <!-- <div class="test">
+      <img src="./img/1.PNG">
+    </div> -->
+    <table>
+      <tr>
+        <td><img src="./img/1.png" alt="logo"></td>
+        <td><img src="./img/2.png" alt="logo"></td>
+        <td><img src="./img/3.png" alt="logo"></td>
+      </tr>
+      <tr>
+        <td><img src="./img/4.png" alt="logo"></td>
+        <td><img src="./img/5.png" alt="logo"></td>
+        <td><img src="./img/6.png" alt="logo"></td>
+      </tr> 
+      <tr>
+        <td><img src="./img/7.png" alt="logo"></td>
+        <td><img src="./img/8.png" alt="logo"></td>
+        <td></td>
+        <!-- <td><img src="./img/9.png" alt="logo"></td> -->
+      </tr>
+      </table>
+  </body>
+  </html>
---
+++ jour03/job03/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  var game = [];
+  let table = $("table");
+  console.log(table);
+});
