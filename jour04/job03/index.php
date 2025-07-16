5aab9a33f462c368a66371cd456b1cf4b8b3d641
Johan <johan.chevet@laplateforme.io>
2025-07-16 14:15:22 +0200

    job03 ~done

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -3,16 +3,22 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <script type="text/javascript" src="./script.js"></script>
     <title>Jour04</title>
   </head>
   <body>
     <form>
-      <input id="id" name="id" placeholder="Id" type="text">
-      <input id="name" name="name" placeholder="Name" type="text">
-      <select>
-        <!-- TODO add option from js -->
+      <label for="id">Id:</label>
+      <input id="id" type="text">
+      <label for="name">Name:</label>
+      <input id="name" type="text">
+      <label for="type">Type:</label>
+      <select id="type">
+        <option value="" selected>Choose here</option>
       </select>
+      <input type="button" id="filter" name="filter" value="filter" onclick="filterPokemon()">
     </form>
+    <!-- <div class="pokemon-list"></div> -->
   </body>
   </html>
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,9 +1,4 @@
-async function onLoad() {
-  // fetch("./pokemon.json").then((response) => {
-  //   response.json().then((data) => {
-  //     console.log(data);
-  //   });
-  // });
+async function setPokemonTypes() {
   const response = await fetch("./pokemon.json");
   const data = await response.json();
   const typeArray = [];
@@ -14,7 +9,82 @@ async function onLoad() {
       }
     });
   });
-  return typeArray;
+  typeArray.forEach((type) => {
+    const option = document.createElement("option");
+    option.innerText = type;
+    document.querySelector("#type").append(option);
+  });
 }
 
-onLoad();
+function parseObjectAndCreateElement(object, elementName) {
+  const objElement = document.createElement("div");
+  objElement.setAttribute("class", elementName);
+  for (const [key, value] of Object.entries(object)) {
+    const keyValueElem = document.createElement("div");
+    if (isNaN(key)) {
+      const elementTitle = document.createElement("span");
+      elementTitle.innerHTML = key + ": ";
+      keyValueElem.append(elementTitle);
+    }
+    let keyElement = undefined;
+    if (typeof value == "number" || typeof value == "string") {
+      keyElement = document.createElement("span");
+      keyElement.setAttribute("class", key);
+      keyElement.innerHTML = value;
+    } else if (typeof value == "object") {
+      keyElement = parseObjectAndCreateElement(value, key);
+    }
+    if (keyElement) {
+      keyValueElem.append(keyElement);
+      objElement.append(keyValueElem);
+    }
+  }
+  return objElement;
+}
+
+function createPokemonList(data) {
+  let pokemonList = document.getElementById("pokemon-list");
+  if (pokemonList) {
+    pokemonList.remove();
+  }
+  pokemonList = document.createElement("div");
+  pokemonList.setAttribute("id", "pokemon-list");
+  data.forEach((pokemon) => {
+    const elem = parseObjectAndCreateElement(pokemon, "pokemon-card");
+    pokemonList.append(elem);
+  });
+  document.body.append(pokemonList);
+}
+
+async function filterPokemon() {
+  const response = await fetch("./pokemon.json");
+  let data = await response.json();
+
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const type = document.getElementById("type").value;
+  if (id.length) {
+    data = data.filter((poke) => poke.id == id);
+  }
+  if (name.length) {
+    data = [
+      ...data.filter((poke) => {
+        for (const [_, value] of Object.entries(poke.name)) {
+          if (value.toLowerCase() == name.toLowerCase()) {
+            return true;
+          }
+        }
+      }),
+    ];
+  }
+  if (type.length) {
+    data = [
+      ...data.filter((poke) => {
+        return poke.type.find((t) => t === type);
+      }),
+    ];
+  }
+  createPokemonList(data);
+}
+
+setPokemonTypes();
